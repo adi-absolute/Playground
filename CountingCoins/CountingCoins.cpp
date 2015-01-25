4151938b17f62d86dfef7803f61f794385f169f4
Adi <adi@absolute-software.co.uk>
2015-01-25 03:12:08 +0000

    using static_cast

--- CountingCoins/CountingCoins.cpp
+++ CountingCoins/CountingCoins.cpp
@@ -4,24 +4,24 @@
 
 using namespace ::std;
 
-static unsigned int CoinValue[(unsigned int)Denominations::NoOfCoins] = { 1, 5, 10, 25 };
+static unsigned int CoinValue[static_cast<unsigned int>(Denominations::NoOfCoins)] = { 1, 5, 10, 25 };
 
 vector<vector<unsigned int>> CountCoins(unsigned int valueInCents)
 {
 	vector<vector<unsigned int>> result;
 	bool end = false;
 	int dropCoinValueLoopNo = DO_NOT_DROP;
-	unsigned int dropCoinValue = (unsigned int)Denominations::NoOfCoins;
-	unsigned int workingCoin = (unsigned int)Denominations::NoOfCoins;
+	unsigned int dropCoinValue = static_cast<unsigned int>(Denominations::NoOfCoins);
+	unsigned int workingCoin = static_cast<unsigned int>(Denominations::NoOfCoins);
 	vector <unsigned int> record;
 
 	while (!end)
 	{
-		vector<unsigned int> thisRound((unsigned int)Denominations::NoOfCoins);
+		vector<unsigned int> thisRound(static_cast<unsigned int>(Denominations::NoOfCoins));
 		int loopNo = 0;
 		
 		unsigned int remainingAmount = valueInCents;
-		unsigned int currentCoin = (unsigned int)Denominations::Quarter;
+		unsigned int currentCoin = static_cast<unsigned int>(Denominations::Quarter);
 		bool recordSwapped = false;
 
 		while (remainingAmount > 0)
@@ -47,12 +47,12 @@ vector<vector<unsigned int>> CountCoins(unsigned int valueInCents)
 			if (loopNo == dropCoinValueLoopNo)
 				currentCoin = dropCoinValue;
 
-			if ((loopNo == 0) && (currentCoin == (unsigned int)Denominations::Penny))
+			if ((loopNo == 0) && (currentCoin == static_cast<unsigned int>(Denominations::Penny)))
 				end = true;
 
 			if (remainingAmount >= CoinValue[currentCoin])
 			{
-				if (currentCoin > (unsigned int)Denominations::Penny)
+				if (currentCoin > static_cast<unsigned int>(Denominations::Penny))
 					workingCoin = currentCoin;
 
 				record.push_back(currentCoin);
@@ -62,12 +62,12 @@ vector<vector<unsigned int>> CountCoins(unsigned int valueInCents)
 			}
 			else 
 			{
-				if (currentCoin > (unsigned int)Denominations::Penny)
+				if (currentCoin > static_cast<unsigned int>(Denominations::Penny))
 					currentCoin--;
 			}
 		}
 
-		if (currentCoin != (unsigned int)Denominations::Penny)
+		if (currentCoin != static_cast<unsigned int>(Denominations::Penny))
 		{
 			dropCoinValueLoopNo = loopNo - 1;
 			dropCoinValue = currentCoin - 1;
@@ -75,7 +75,7 @@ vector<vector<unsigned int>> CountCoins(unsigned int valueInCents)
 		else
 		{
 			dropCoinValue = workingCoin - 1;
-			dropCoinValueLoopNo = loopNo - thisRound[(unsigned int)Denominations::Penny] - 1;
+			dropCoinValueLoopNo = loopNo - thisRound[static_cast<unsigned int>(Denominations::Penny)] - 1;
 		}
 		
 		result.push_back(thisRound);
