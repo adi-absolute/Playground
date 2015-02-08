5cb4744c5a59d593e54aac792cbbc05e39d56449
Adi <adi@absolute-software.co.uk>
2015-02-08 12:37:28 +0000

    user input for grid and live cells

--- GameOfLife/GameOfLifeApp/main.cpp
+++ GameOfLife/GameOfLifeApp/main.cpp
@@ -9,9 +9,9 @@ using namespace ::std;
 
 void PrintGrid(vector<vector<CellState>>& grid)
 {
-	for (int i = 0; i < ROWS; i++)
+	for (int i = 0; i < grid.size(); i++)
 	{
-		for (int j = 0; j < COLS; j++)
+		for (int j = 0; j < grid[0].size(); j++)
 		{
 			if (grid[i][j] == CellState::Alive)
 				cout << "O";
@@ -25,20 +25,28 @@ void PrintGrid(vector<vector<CellState>>& grid)
 
 void main()
 {
-	Grid game(ROWS, COLS);
+	//Grid game(ROWS, COLS);
 	bool exit = false;
+/*
+	game->SetCellAlive(0, 1);
+	game->SetCellAlive(1, 2);
+	game->SetCellAlive(2, 0);
+	game->SetCellAlive(2, 1);
+	game->SetCellAlive(2, 2);
+*/
+	int w;
+	cout << "Enter width/height of grid: ";
+	cin >> w;
 
-	game.SetCellAlive(4, 4);
-	game.SetCellAlive(5, 4);
-	game.SetCellAlive(3, 4);
+	Grid *game = new Grid(w, w);
 
-	vector<vector<CellState>>& grid = game.GetState();
+	vector<vector<CellState>>& grid = game->GetState();
 	cout << "Starting State:" << endl;
 	PrintGrid(grid);
 
 	while (!exit)
 	{
-		cout << "Press n to select number of runs, x to exit: ";
+		cout << "Press s to set a cell alive, n to select number of runs, x to exit: ";
 		char x = '0';
 		cin >> x;
 
@@ -50,10 +58,19 @@ void main()
 
 			for (int i = 0; i < n; i++)
 			{
-				game.Run();
+				game->Run();
 				PrintGrid(grid);
 				_sleep(500);
 			}
+		}
+		else if (x == 's')
+		{
+			cout << "X & Y position of live cell: ";
+			int r, c;
+			cin >> r >> c;
+			game->SetCellAlive(r, c);
+			cout << "New State:" << endl;
+			PrintGrid(grid);
 		}
 		else if (x == 'x')
 		{
