d336230f727248a5e6d9408bbb50b3a4fb23d4d1
Adi <adi@absolute-software.co.uk>
2015-11-11 16:52:07 +0000

    Creating new files for second pass lexer

---
+++ CodeMetricsAnalyser/CodeMetricsAnalyser/SecondPassLexerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeMetricsAnalyser;
+using System.IO;
+using System.Collections.Generic;
+
+namespace TestsForCodeMetricsAnalyser
+{
+    class SecondPassLexerTests
+    {
+
+        public void Lex_simple_hash_include_token()
+        {/*
+           stringstream ss;
+           ss << "#include";
+
+           Token expected = new Token(0, 0);
+           expected.Text = "#include";
+           List<Token> input;
+           input.Add(expected);
+
+           token_list output = secondPassLexer.SplitTokens(input);
+   
+           ASSERT_TOKENLISTS_IDENTICAL(input, output);*/
+        }
+    }
+}
