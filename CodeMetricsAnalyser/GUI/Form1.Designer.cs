4de4d6a99a2a65bf745b7d2d2e24f17b1b915914
Adi <adi@absolute-software.co.uk>
2015-11-19 17:25:15 +0000

    Simple GUI currently outputs number of lines in a given file

---
+++ CodeMetricsAnalyser/GUI/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1_Analyse = new System.Windows.Forms.Button();
+            this.label_numberOfLines = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1_Analyse
+            // 
+            this.button1_Analyse.Location = new System.Drawing.Point(102, 89);
+            this.button1_Analyse.Name = "button1_Analyse";
+            this.button1_Analyse.Size = new System.Drawing.Size(75, 23);
+            this.button1_Analyse.TabIndex = 0;
+            this.button1_Analyse.Text = "Analyse File";
+            this.button1_Analyse.UseVisualStyleBackColor = true;
+            this.button1_Analyse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label_numberOfLines
+            // 
+            this.label_numberOfLines.AutoSize = true;
+            this.label_numberOfLines.Location = new System.Drawing.Point(109, 161);
+            this.label_numberOfLines.Name = "label_numberOfLines";
+            this.label_numberOfLines.Size = new System.Drawing.Size(29, 13);
+            this.label_numberOfLines.TabIndex = 1;
+            this.label_numberOfLines.Text = "label";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label_numberOfLines);
+            this.Controls.Add(this.button1_Analyse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1_Analyse;
+        private System.Windows.Forms.Label label_numberOfLines;
+    }
+}
+
---
+++ CodeMetricsAnalyser/GUI/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using CodeMetricsAnalyser;
+
+namespace GUI
+{
+    public partial class Form1 : Form
+    {
+        CommentStringLexer commentStringLexer;
+        SecondPassLexer secondPassLexer;
+        public int lines = 0;
+
+        private void UpdateNumberOfLines()
+        {
+            label_numberOfLines.Text = lines.ToString();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            UpdateNumberOfLines();
+
+            commentStringLexer = new CommentStringLexer();
+            secondPassLexer = new SecondPassLexer();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the open file dialog box.
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            // Set filter options and filter index.
+            openFileDialog1.Filter = "Text Files (.txt)|*.txt|All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+
+            openFileDialog1.Multiselect = true;
+
+            // Call the ShowDialog method to show the dialog box.
+            var userClickedOK = openFileDialog1.ShowDialog();
+
+            // Process input if the user clicked OK.
+            if (userClickedOK == DialogResult.OK)
+            {
+                // Open the selected file to read.
+                System.IO.Stream fileStream = openFileDialog1.OpenFile();
+
+                var c = commentStringLexer.GenerateTokens(fileStream);
+                var s = secondPassLexer.SplitTokens(c);
+
+                lines = commentStringLexer.NumberOfLines;
+
+                UpdateNumberOfLines();
+                
+                fileStream.Close();
+            }
+        }
+    }
+}
